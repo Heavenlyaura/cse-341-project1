ef987f0f166a8b9180b86ade7dcc46e5668c0ec7
heavenlyAura <goodnessokafor462@gmial.com>
2024-09-18 20:19:05 +0100

    "Added new root route to return 'Hello World' and updated existing contact routes"

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { sendAllContacts, sendSingleContact } = require("../controllers/index");
 
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
 router.get("/contact", sendAllContacts);
 router.get("/contact/:ID", sendSingleContact);
 
